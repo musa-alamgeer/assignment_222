1a16f71d3e8cfe1a173b6cba93e2f46046125251
Musa Alamgeer <u2017352@giki.edu.pk>
2020-11-01 02:26:58 +0500

    text changes

--- reactapp2/src/App.js
+++ reactapp2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Musa Alamgeer 2017352 -
         </p>
         <a
           className="App-link"
